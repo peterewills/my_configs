84b51478f4fe06227208312b79966d9be4ca1c24
Peter Wills <peter.e.wills@gmail.com>
2020-10-29 10:52:18 -0400

    update zenburn plots

--- zenburn_plots.py
+++ zenburn_plots.py
@@ -30,65 +30,102 @@ zenburn-themed. This is useful when running Emacs Ipython Notebook.
 ### Change matplotlib defaults ###
 ##################################
 
-def zenburn_plots():
+
+def zenburn_plots(log_info=True):
 
     import matplotlib as mpl
     from cycler import cycler
-    # import seaborn as sns
-    
+    from matplotlib import pyplot as plt
+
+    plt.rc("figure", figsize=(12, 8))
+    plt.rc("font", size=14)
+
     # To restore defaults, run `mpl.rcdefaults()`.
     # For nice plots, use `plt.style.use(['classic','ggplot'])`.
 
-    # colors used for plotting (zenburn styled)
-    red,green,blue,orange = '#A55D5D', '#466F46','#4A7274','#A35E2E'
-    black,d_gray,l_gray,ll_gray = '#3F3F3F','#6F6F6F','#9F9F9F','#CFCFCF'
+    from enum import Enum
+
+    class ZenburnColors(Enum):
+        RED = "#A55D5D"
+        GREEN = "#466F46"
+        BLUE = "#4A7274"
+        ORANGE = "#A35E2E"
+        BLACK = "#3F3F3F"
+        DARK_GREY = "#6F6F6F"
+        GREY = "#9F9F9F"
+        LIGHT_GREY = "#CFCFCF"
 
     # there's a nicer way to do this, but whatever
     # General Settings
-    mpl.rcParams['legend.loc'] = 'best'
-    mpl.rcParams['axes.titlesize'] = '18'
-    mpl.rcParams['figure.figsize'] = [8,6]
-    mpl.rcParams['axes.grid'] = True
-    mpl.rcParams['axes.labelsize'] = '14'
-    
-    # Zenburn Color Settings
-    mpl.rcParams['text.color'] = 'white'
-    mpl.rcParams['legend.facecolor'] = l_gray
-    mpl.rcParams['legend.fancybox'] = True
-    mpl.rcParams['lines.color'] = black
-    mpl.rcParams['axes.prop_cycle'] = cycler('color',
-                                             [red,green,blue,
-                                              orange,black])
-    mpl.rcParams['patch.facecolor'] = green
-    mpl.rcParams['axes.facecolor'] = ll_gray
-    mpl.rcParams['axes.labelcolor'] = 'white'
-    mpl.rcParams['xtick.color'] = ll_gray
-    mpl.rcParams['ytick.color'] = ll_gray
+    mpl.rcParams["legend.loc"] = "best"
+    mpl.rcParams["axes.titlesize"] = "18"
+    mpl.rcParams["figure.figsize"] = [8, 6]
+    mpl.rcParams["axes.grid"] = True
+    mpl.rcParams["axes.labelsize"] = "14"
 
+    # Zenburn Color Settings
+    mpl.rcParams["text.color"] = "white"
+    mpl.rcParams["legend.facecolor"] = ZenburnColors.GREY
+    mpl.rcParams["legend.fancybox"] = True
+    mpl.rcParams["lines.color"] = ZenburnColors.BLACK
+    mpl.rcParams["axes.prop_cycle"] = cycler(
+        "color",
+        [
+            ZenburnColors.RED,
+            ZenburnColors.GREEN,
+            ZenburnColors.BLUE,
+            ZenburnColors.ORANGE,
+            ZenburnColors.BLACK,
+        ],
+    )
+    mpl.rcParams["patch.facecolor"] = ZenburnColors.GREEN
+    mpl.rcParams["axes.facecolor"] = ZenburnColors.LIGHT_GREY
+    mpl.rcParams["axes.labelcolor"] = "white"
+    mpl.rcParams["xtick.color"] = ZenburnColors.LIGHT_GREY
+    mpl.rcParams["ytick.color"] = ZenburnColors.LIGHT_GREY
 
     ###############################
     ### Change seaborn defaults ###
     ###############################
-    
-    # # modify axes style
-    # sns.set_style({
-    #     'axes.edgecolor': black,
-    #     'axes.facecolor': ll_gray,
-    #     'figure.facecolor' : black,
-    #     'axes.labelcolor' : ll_gray,
-    #     'ytick.color' : ll_gray,
-    #     'xtick.color' : ll_gray,
-    #     'grid.color' : l_gray
-    #     #    'legend.color' : l_gray
-    # })
 
-    # # change color palette for plotting
-    # sns.set_palette([red,green,blue,orange,black])
+    # right now, we can't do this. It results in an ImportError, because
+    # "python is not installed as a framework." So...
+
+    import seaborn as sns
+
+    # modify axes style
+    sns.set_style(
+        {
+            "axes.edgecolor": ZenburnColors.BLACK,
+            "axes.facecolor": ZenburnColors.LIGHT_GREY,
+            "figure.facecolor": ZenburnColors.BLACK,
+            "axes.labelcolor": ZenburnColors.LIGHT_GREY,
+            "ytick.color": ZenburnColors.LIGHT_GREY,
+            "xtick.color": ZenburnColors.LIGHT_GREY,
+            "grid.color": ZenburnColors.GREY
+            #    'legend.color' : ZenburnColors.GREY
+        }
+    )
+
+    # change color palette for plotting
+    sns.set_palette(
+        [
+            ZenburnColors.RED,
+            ZenburnColors.GREEN,
+            ZenburnColors.BLUE,
+            ZenburnColors.ORANGE,
+            ZenburnColors.BLACK,
+        ]
+    )
+
+    if log_info:
+        #######################################
+        ### Print message informing imports ###
+        #######################################
+
+        print("")
+        print("Plots have been zenburned!")
 
 
-    #######################################
-    ### Print message informing imports ###
-    #######################################
-
-    print('')
-    print('Plots have been zenburned!')
+# change the plotstyle when you're running shit locally
+zenburn_plots(log_info=False)
